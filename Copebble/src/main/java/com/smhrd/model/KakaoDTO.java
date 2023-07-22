965608c030befa6344e9a206619152ef23ba8269
김지혜 <134493927+rlawlgp0197@users.noreply.github.com>
2023-07-22 16:14:56 +0900

    Add files via upload
    2023-07-22/김지혜/파일업로드

---
+++ Copebble/src/main/java/com/smhrd/model/KakaoDTO.java
@@ -0,0 +1,17 @@
+package com.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class KakaoDTO {
+
+	private String ID;
+	private String NAME;
+	
+}
