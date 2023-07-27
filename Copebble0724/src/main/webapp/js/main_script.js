356e503a150dd8d0c83d10e65014ed71a81daf41
smhrd <smhrd@220.80.88.1>
2023-07-27 11:22:58 +0900

    230727 11:22 이승훈 자바스크립트,css 코드 수정 테스트용 푸시

--- Copebble0724/src/main/webapp/js/main_script.js
+++ Copebble0724/src/main/webapp/js/main_script.js
@@ -67,7 +67,6 @@ document.addEventListener('DOMContentLoaded', function() {
 			// 팝업 창 표시
 
 			document.getElementById('addEventPopup').style.display = 'block';
-			// 시작일과 종료일 입력 필드 초기화
 			document.getElementById('eventStart').value = info.dateStr;
 			document.getElementById('eventEnd').value = info.dateStr;
 		},
@@ -148,8 +147,8 @@ document.addEventListener('DOMContentLoaded', function() {
 				title: eventTitle,
 				start: eventStart,
 				end: eventEnd,
-				color: eventColor, // 일정의 색상을 사용자가 선택한 색상으로 지정
-				textColor: '#000000', // 일정의 글자색을 검은색으로 지정
+				color: eventColor,
+				textColor: '#000000',
 				allDay: true
 			};
 
@@ -272,151 +271,6 @@ year = date.getFullYear();
 month = date.getMonth() + 1;
 day = date.getDate();
 document.getElementById("current_date").innerHTML = month + "월 " + day + "일";
-/*
-// familynotice_add 버튼에 클릭 이벤트 리스너 추가
-$('.familynotice_add').click(function() {
-	// addEventPopup2 요소의 가시성을 토글합니다.
-	$('#addEventPopup2').toggle();
-});
-
-// 일정 추가 버튼 이벤트 처리
-$('#addTodoBtn').click(function() {
-	const eventTodoTitle = $('#eventTodoTitle').val();
-	const eventTodoStart = $('#eventTodoStart').val();
-	const eventTodoEnd = $('#eventTodoEnd').val();
-	const eventTodoManager = $('input[name=Manager]:checked').val();
-
-
-	// AJAX를 사용하여 데이터 서버에 보내기
-	$.ajax({
-		type: 'POST',
-		url: './todoData',
-		data: { "title": eventTodoTitle, 'start': eventTodoStart, 'end': eventTodoEnd, 'manager': eventTodoManager },
-		dataType: 'text',
-		success: function(response) {
-
-			console.log('데이터 전송 성공!!!');
-
-		},
-		error: function(xhr, status, error) {
-			console.error('데이터를 서버에 전송하는 중 오류 발생:', error);
-
-		}
-	});
-
-	// To-do 리스트에 새로운 항목 추가
-	const todoItem = `
-		<li>
-			<input type="checkbox"/>
-			${eventTodoManager} ${eventTodoTitle}
-			${eventTodoStart} ~ ${eventTodoEnd}
-			<button class="deleteTodoBtn">X</button>
-		</li>
-	`;
-	$('#todoList').append(todoItem);
-
-	// 일정 추가 창 닫기
-	$('#addEventPopup2').hide();
-	// 입력 필드 초기화
-	document.getElementById('eventTodoTitle').value = '';
-	document.getElementById('eventTodoStart').value = '';
-	document.getElementById('eventTodoEnd').value = '';
-	$('input[name=Manager]').prop('checked', false);
-});
-// 삭제 버튼 클릭 이벤트 처리
-$(document).on('click', '.deleteTodoBtn', function() {
-	// 클릭한 삭제 버튼이 속한 항목을 To-do 리스트에서 제거합니다.
-	$(this).parent().remove();
-});
-
-// 취소 버튼 이벤트 처리
-$('#cancelTodoBtn').click(function() {
-	// 일정 추가 창 닫기
-	$('#addEventPopup2').hide();
-	// 입력 필드 초기화
-	document.getElementById('eventTodoTitle').value = '';
-	document.getElementById('eventTodoStart').value = '';
-	document.getElementById('eventTodoEnd').value = '';
-	$('input[name=Manager]').prop('checked', false);
-});
-
-
-
-// 가족알림장 리스트 생성
-function createTodoItem(text, date) {
-	const li = document.createElement('li');
-	li.classList.add('todo-item'); // 클래스 이름을 'todo-item'으로 설정
-	li.draggable = true; // 리스트 아이템을 드래그 가능하도록 설정
-	li.innerHTML = `
-		<input type="checkbox" />
-		${text}
-		${date}
-		<button class="deleteTodoBtn">X</button>
-	`;
-	return li;
-}
-
-// 새로운 할일 리스트 아이템을 리스트에 추가하는 함수
-function addTodoItem(text, date) {
-	const todoList = document.getElementById('todoList');
-	const newItem = createTodoItem(text, date);
-	todoList.appendChild(newItem);
-}
-// DB에 저장된 리스트 불러오기!
-addTodoItem("남자 나는 탈주 할것이다!", "07-22 ~ 07-31");
-addTodoItem("남자 아무것도하기싫다!", "07-20 ~ 07-31");
-
-//가족알림장 드래그앤드롭
-let draggedItem = null;
-
-// 드래그 앤 드롭 기능 구현
-$(document).off('dragstart', '.todo-item').on('dragstart', '.todo-item', function(e) {
-	draggedItem = e.target;
-	e.originalEvent.dataTransfer.setData('text/plain', ''); // 드래그 이벤트를 동작하기 위해 필요
-});
-
-$(document).off('dragover', '.todo-item').on('dragover', '.todo-item', function(e) {
-	e.preventDefault();
-});
-
-$(document).off('drop', '.todo-item').on('drop', '.todo-item', function(e) {
-	e.preventDefault();
-	const dropTarget = e.target;
-
-	// 만약 드롭한 요소가 리스트 아이템이면 해당 위치로 이동
-	if ($(dropTarget).hasClass('todo-item')) {
-		const dropIndex = $(dropTarget).index();
-		const draggedIndex = $(draggedItem).index();
-
-		if (dropIndex > draggedIndex) {
-			$(dropTarget).after(draggedItem);
-		} else {
-			$(dropTarget).before(draggedItem);
-		}
-	}
-});
-
-$(document).ready(function() {
-	$('#addTodoBtn').click(function() {
-		const eventTodoTitle = $('#eventTodoTitle').val();
-		const eventTodoStart = $('#eventTodoStart').val();
-		const eventTodoEnd = $('#eventTodoEnd').val();
-		const eventTodoManager = $('input[name=Manager]:checked').val();
-
-		// To-do 리스트에 새로운 항목 추가
-		const todoItem = `
-	  <li class="todo-item">
-		<input type="checkbox"/>
-		${eventTodoManager} ${eventTodoTitle}
-		${eventTodoStart} ~ ${eventTodoEnd}
-		<button class="deleteTodoBtn">X</button>
-	  </li>
-	`;
-		$('#todoList').append(todoItem);
-	});
-});
-
-
 
 //////////////////////////메인 박스 색깔 랜덤
 const myBox = document.getElementById('myBox');
@@ -444,40 +298,5 @@ function getRandomYellowColor() {
 	const randomIndex = Math.floor(Math.random() * yellowColors.length);
 	return yellowColors[randomIndex];
 }
-///////////////////////가족알림장 리스트 색깔 랜덤
-
-const colors = [
-	'#ffc6a5',
-	'#fcf6dd',
-	'#FFFFE6',
-	'#ffd895',
-	'#ffe3a3',
-	'#FFFFF9',
-	'#FFFFFC',
-	'#FFFFFE',
-	'#FFDD7C',
-	'#FEFEF0'
-];
-
-// 가족알림장 리스트 색상 랜덤 출력
-function getRandomColorFromArray(colorArray) {
-	const randomIndex = Math.floor(Math.random() * colorArray.length);
-	return colorArray[randomIndex];
-}
-
-// 여기서 특정 색깔만 출력할 예정!
-function applyRandomColorToElement(element) {
-	const randomColor = getRandomColorFromArray(colors);
-	element.style.backgroundColor = randomColor;
-}
-
-// 페이지 로드 후 실행되는 함수
-document.addEventListener("DOMContentLoaded", function() {
-	const todoItems = document.querySelectorAll(".todo-item");
-	todoItems.forEach(function(item) {
-		applyRandomColorToElement(item);
-	});
-});
 
 
-*/
--- Copebble0724/src/main/webapp/js/main_script_fa.js
+++ Copebble0724/src/main/webapp/js/main_script_fa.js
@@ -1,134 +1,169 @@
 $(document).ready(function() {
-  const colors = [
-    '#ffc6a5',
-    '#fcf6dd',
-    '#FFFFE6',
-    '#ffd895',
-    '#ffe3a3',
-    '#FFFFF9',
-    '#FFFFFC',
-    '#FFFFFE',
-    '#FFDD7C',
-    '#FEFEF0'
-  ];
-
-  // Apply random color to todo items
-  function applyRandomColorToElement(element) {
-    const randomColor = getRandomColorFromArray(colors);
-    element.style.backgroundColor = randomColor;
-  }
-
-  // Helper function to get random color from the array
-  function getRandomColorFromArray(colorArray) {
-    const randomIndex = Math.floor(Math.random() * colorArray.length);
-    return colorArray[randomIndex];
-  }
-
-  // Event delegation for add and delete buttons
-  $(document).on('click', '.familynotice_add', function() {
-    $('#addEventPopup2').toggle();
-  });
-
-  $(document).on('click', '#addTodoBtn', function() {
-    const eventTodoTitle = $('#eventTodoTitle').val();
-    const eventTodoStart = $('#eventTodoStart').val();
-    const eventTodoEnd = $('#eventTodoEnd').val();
-    const eventTodoManager = $('input[name=Manager]:checked').val();
-
-    // AJAX request to send data to the server
-    $.ajax({
-      type: 'POST',
-      url: './todoData',
-      data: { "title": eventTodoTitle, 'start': eventTodoStart, 'end': eventTodoEnd, 'manager': eventTodoManager },
-      dataType: 'text',
-      success: function(response) {
-        console.log('Data sent successfully!!!');
-      },
-      error: function(xhr, status, error) {
-        console.error('Error sending data to the server:', error);
-      }
-    });
-
-    // To-do list item template
-    const todoItem = `
-      <li class="todo-item">
-        <input type="checkbox"/>
-        ${eventTodoManager} ${eventTodoTitle}
-        ${eventTodoStart} ~ ${eventTodoEnd}
-        <button class="deleteTodoBtn">X</button>
-      </li>
-    `;
-
-    // Append the new item to the todo list
-    $('#todoList').append(todoItem);
-
-    // Apply random color to the new todo item
-    const newTodoItem = $('#todoList li').last()[0];
-    applyRandomColorToElement(newTodoItem);
-
-    // Close the add event popup and reset input fields
+  // 함수로 입력 필드 초기화와 일정 추가 창 닫기를 처리하는 공통 코드 분리
+  function hideAddEventPopup() {
     $('#addEventPopup2').hide();
     $('#eventTodoTitle').val('');
     $('#eventTodoStart').val('');
     $('#eventTodoEnd').val('');
     $('input[name=Manager]').prop('checked', false);
-  });
-
-  $(document).on('click', '.deleteTodoBtn', function() {
-    // Remove the clicked todo item from the list
-    $(this).parent().remove();
-  });
-
-  // Main box random color
-  const myBox = document.getElementById('myBox');
-  myBox.addEventListener('click', () => {
-    const newColor = getRandomYellowColor();
-    myBox.style.backgroundColor = newColor;
-  });
-
-  // Helper function to get random yellow color
-  function getRandomYellowColor() {
-    const yellowColors = [
-      '#ffc6a5',
-      '#fcf6dd',
-      '#FFFFE6',
-      '#ffd895',
-      '#ffe3a3',
-      '#FFFFF9',
-      '#FFFFFC',
-      '#FFFFFE',
-      '#FFDD7C',
-      '#FEFEF0'
-    ];
-    const randomIndex = Math.floor(Math.random() * yellowColors.length);
-    return yellowColors[randomIndex];
   }
 
-  // DB data retrieval and rendering (for demonstration)
-  //addTodoItem("안녕하신가?", "07-22 ~ 07-31");
-});
+function loadTodoList(lastTimestamp) {
+  $.ajax({
+    type: 'POST',
+    url: './todoSelectCon',
+    data: { lastTimestamp: lastTimestamp }, // 타임스탬프를 서버에 전송합니다.
+    dataType: 'json',
+    success: function(response) {
+      console.log('알림장 조회 성공', response);
+      $('.todoList').empty(); // 기존 목록 삭제
+      for (let i = 0; i < response.length; i++) {
+        const startDate = new Date(response[i].start);
+        const endDate = new Date(response[i].end);
 
-$(document).on('dragstart', '.todo-item', function(e) {
-  draggedItem = e.target;
-  e.originalEvent.dataTransfer.setData('text/plain', '');
-});
+        const startMonth = String(startDate.getMonth() + 1).padStart(2, '0');
+        const startDay = String(startDate.getDate()).padStart(2, '0');
 
-$(document).on('dragover', '.todo-item', function(e) {
-  e.preventDefault();
-});
+        const endMonth = String(endDate.getMonth() + 1).padStart(2, '0');
+        const endDay = String(endDate.getDate()).padStart(2, '0');
 
-$(document).on('drop', '.todo-item', function(e) {
-  e.preventDefault();
-  const dropTarget = e.target;
+        const formattedStart = `${startMonth}-${startDay}`;
+        const formattedEnd = `${endMonth}-${endDay}`;
 
-  if ($(dropTarget).hasClass('todo-item')) {
-    const dropIndex = $(dropTarget).index();
-    const draggedIndex = $(draggedItem).index();
-
-    if (dropIndex > draggedIndex) {
-      $(dropTarget).after(draggedItem);
-    } else {
-      $(dropTarget).before(draggedItem);
+        const todoItem = `
+          <div class="memo_box">
+            <div>${response[i].WHO}</div>
+            <div>${response[i].title}</div>
+            <div>${formattedStart}</div>
+            <div>${formattedEnd}</div>
+            <button class="Todo_delete_btn">X</button>
+          </div>
+        `;
+        $('.todoList').append(todoItem); // 새로운 목록 추가
+      }
+    },
+    error: function(error) {
+      console.error('알림장 조회 실패', error);
     }
-  }
+  });
+}
+
+
+
+  // 페이지가 로드될 때 할 일 목록을 서버에서 가져와 화면에 보여줌
+  loadTodoList();
+  
+  // 5초마다 업데이트를 확인하는 폴링(polling) 설정
+  setInterval(function() {
+    // 기존 목록에서 마지막 타임스탬프를 찾아서 서버에 보냅니다.
+    const lastTimestamp = $('.todoList').find('.memo_box:last-child div:eq(3)').text().trim();
+    loadTodoList(lastTimestamp);
+  }, 1000); // 원하는 간격으로 인터벌을 조정합니다. (예: 이 예제에서는 5초)
+
+  // '가족알림장 추가' 버튼 클릭 이벤트 리스너
+  $('.familynotice_add').click(function() {
+    $('#addEventPopup2').toggle();
+  });
+
+  // 일정 추가 버튼 이벤트 처리
+  $('#addTodoBtn').click(function() {
+    const title = $('#eventTodoTitle').val();
+    const start = $('#eventTodoStart').val();
+    const end = $('#eventTodoEnd').val();
+    const manager = $('input[name=Manager]:checked').val();
+
+    // AJAX를 사용하여 데이터 서버에 보내기
+    $.ajax({
+      type: 'POST',
+      url: './todoDataCon',
+      data: { "title": title, 'start': start, 'end': end, 'manager': manager },
+      dataType: 'text',
+      success: function(response) {
+   
+      },
+      error: function(xhr, status, error) {
+        console.error('데이터를 서버에 전송하는 중 오류 발생:', error);
+      }
+    });
+	
+	loadTodoList()
+	
+    // To-do 리스트에 새로운 항목 추가 using jQuery
+    const todoItem = `
+      <div class="memo_box">
+        <div>${manager}</div>
+        <div>${title}</div>
+        <div>${start}</div>
+        <div>${end}</div>
+        <button class="Todo_delete_btn">X</button>
+      </div>
+    `;
+    $('.todoList').append(todoItem);
+
+    // 일정 추가 창 닫기 및 입력 필드 초기화
+    hideAddEventPopup();
+  });
+
+  // 삭제 버튼 클릭 이벤트 처리
+  $(document).on('click', '.Todo_delete_btn', function() {
+    const listItem = $(this).closest('.memo_box');
+    const name = listItem.find('div').eq(0).text().trim();
+    const title = listItem.find('div').eq(1).text().trim();
+    const start = listItem.find('div').eq(2).text().trim();
+    const end = listItem.find('div').eq(3).text().trim();
+	
+    // 서버에 해당 리스트의 정보를 삭제 요청합니다.
+    $.ajax({
+      type: 'POST',
+      url: './todoDeleteCon',
+      data: { "name": name, "title": title, 'start': start, 'end': end },
+      dataType: 'text',
+      success: function(response) {
+        console.log('가족알림장 데이터 삭제 성공!!!');
+        listItem.remove(); // 화면에서도 해당 항목 삭제
+      },
+      error: function(xhr, status, error) {
+        console.error('가족알림장 데이터 삭제 실패', error);
+      }
+    });
+  });
+
+  // 취소 버튼 이벤트 처리
+  $('#cancelTodoBtn').click(function() {
+    hideAddEventPopup();
+  });
 });
+
+var todoPopupIsDragging = false;
+var todoPopupDragX = 960;
+var todoPopupDragY = 480;
+var todoPopupOffsetX = 0;
+var todoPopupOffsetY = 0;
+var todoPopupEl = document.getElementById('addEventPopup2');
+
+// deleteEventPopup 팝업 창 드래그 시작
+function todoPopupStartDrag(e) {
+	todoPopupIsDragging = true;
+	todoPopupOffsetX = e.clientX - todoPopupDragX;
+	todoPopupOffsetY = e.clientY - todoPopupDragY;
+}
+
+// deleteEventPopup 팝업 창 드래그 중
+function todoPopupDrag(e) {
+	if (todoPopupIsDragging) {
+		todoPopupDragX = e.clientX - todoPopupOffsetX;
+		todoPopupDragY = e.clientY - todoPopupOffsetY;
+		todoPopupEl.style.left = todoPopupDragX + 'px';
+		todoPopupEl.style.top = todoPopupDragY + 'px';
+	}
+}
+
+// deleteEventPopup 팝업 창 드래그 종료
+function todoPopupEndDrag() {
+	todoPopupIsDragging = false;
+}
+
+// deleteEventPopup 팝업 창 드래그 이벤트 리스너 등록
+todoPopupEl.addEventListener('mousedown', todoPopupStartDrag);
+document.addEventListener('mousemove', todoPopupDrag);
+document.addEventListener('mouseup', todoPopupEndDrag);
